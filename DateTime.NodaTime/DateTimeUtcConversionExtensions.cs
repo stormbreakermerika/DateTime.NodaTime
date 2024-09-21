a5221c655c68fd3bd15aa8504efd07ef0114e7f2
stormbreakermerika <benjamin.t.kaiser@outlook.com>
2024-09-20 21:04:10 -0600

    Add project files.

---
+++ DateTime.NodaTime/DateTimeUtcConversionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using NodaTime;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DateTime.NodaTime
+{
+    public static class DateTimeUtcConversionExtensions
+    {
+        public static System.DateTime LocalToUtc(this System.DateTime dateTime, string ianaTimeZone)
+        {
+            ZonedDateTime localDateTime = LocalDateTime.FromDateTime(dateTime).InZoneLeniently(DateTimeZoneProviders.Tzdb[ianaTimeZone]);
+            Instant instant = localDateTime.ToInstant();
+            return instant.ToDateTimeUtc();
+        }
+
+        public static System.DateTime UtcToLocal(this System.DateTime dateTime, string ianaTimeZone)
+        {
+            Instant utcInstant = Instant.FromDateTimeUtc(dateTime);
+            ZonedDateTime localDateTime = utcInstant.InZone(DateTimeZoneProviders.Tzdb[ianaTimeZone]);
+            return localDateTime.ToDateTimeUnspecified();
+        }
+    }
+}
